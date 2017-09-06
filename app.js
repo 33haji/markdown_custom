6d66f7fb832e862b62a8196c723dbe181d8c1447
33haji <one_for_all_0828@yahoo.co.jp>
2017-09-06 23:41:33 +0900

    angular.jsをインストール

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.engine('htm', require('ejs').renderFile);
+app.engine('html', require('ejs').renderFile);
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -40,7 +42,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error.html');
 });
 
 module.exports = app;
