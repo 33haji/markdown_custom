8efc6b8f3c9b8537aa5f97a62c77e653a118aec4
33haji <one_for_all_0828@yahoo.co.jp>
2017-09-18 22:27:17 +0900

    h2とcodeのMarkdownを実装

--- public/javascripts/header.js
+++ public/javascripts/header.js
@@ -9,6 +9,13 @@ $(function(){
         $("#sidebar-area-content").slideUp('fast');
     }
   });
+  $(document).on('click touchend', function(event) {
+    // メニュー外をクリックした時にメニューを閉じる
+    if (!$(event.target).closest('#sidebar-area-content').length
+        && !$(event.target).closest('#sidebar').length) {
+      $("#sidebar-area-content").slideUp('fast');
+    }
+  });
 
   // ハンバーガーメニュー内のメニューの処理
   $(".ui.dropdown").hover(function(){
@@ -21,24 +28,69 @@ $(function(){
   });
 
   // Markdownのcssの変更(実際の変更処理はindex.js)
+  initializeMarkdown();
   // 見出し1(h1)
-  window.sessionStorage.setItem(['h1'],['default']);
   $(".h1-default").on('click', function(){
-    window.sessionStorage.setItem(['h1'],['default']);
-    refreshMarkdown();
+    editMarkdownCss("h1", "default");
   });
   $(".h1-red").on('click', function(){
-    window.sessionStorage.setItem(['h1'],['red']);
-    refreshMarkdown();
+    editMarkdownCss("h1", "red");
   });
   $(".h1-underline").on('click', function(){
-    window.sessionStorage.setItem(['h1'],['underline']);
-    refreshMarkdown();
+    editMarkdownCss("h1", "underline");
   });
   $(".h1-leftLine").on('click', function(){
-    window.sessionStorage.setItem(['h1'],['leftLine']);
-    refreshMarkdown();
+    editMarkdownCss("h1", "leftLine");
   });
+  // 見出し2(h2)
+  $(".h2-default").on('click', function(){
+    editMarkdownCss("h2", "default");
+  });
+  $(".h2-blue").on('click', function(){
+    editMarkdownCss("h2", "blue");
+  });
+  $(".h2-underline").on('click', function(){
+    editMarkdownCss("h2", "underline");
+  });
+  $(".h2-leftLine").on('click', function(){
+    editMarkdownCss("h2", "leftLine");
+  });
+  // コード
+  $(".code-default").on('click', function(){
+    editMarkdownCss("code", "default");
+  });
+  $(".code-red").on('click', function(){
+    editMarkdownCss("code", "red");
+  });
+  $(".code-green").on('click', function(){
+    editMarkdownCss("code", "green");
+  });
+  $(".code-blue").on('click', function(){
+    editMarkdownCss("code", "blue");
+  });
+  // 初期化
+  function initializeMarkdown(){
+    // 見出し(h1)
+    window.sessionStorage.setItem(['h1'],['default']);
+    $(".h1-default").addClass('active');
+    // 見出し2(h2)
+    window.sessionStorage.setItem(['h2'],['default']);
+    $(".h2-default").addClass('active');
+    // コード
+    window.sessionStorage.setItem(['code'],['default']);
+    $(".code-default").addClass('active');
+  }
+  // 変更処理
+  function editMarkdownCss(tag, element){
+    window.sessionStorage.setItem([tag],[element]);
+    addActive(tag, "."+ tag +"-"+ element);
+    refreshMarkdown();
+  }
+  // メニューにavtiveクラスを付与
+  function addActive(tag, selector){
+    $("[class*='"+ tag +"-']").removeClass('active');
+    $(selector).addClass('active');
+  }
   // メニューをクリック直後にstyleを反映させるための処理
   function refreshMarkdown(){
     var tmp = $('#output-markdown-contents').html();
--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -28,7 +28,7 @@ $(function() {
       case 'leftLine':
         h1DefaultCss();
         $('.output-markdown h1').css({'border-left': 'solid 5px #f2711c',
-                  'padding': '0.25em 0.5em'});
+                                      'padding': '0.25em 0.5em'});
         break;
       default:
         h1DefaultCss();
@@ -40,6 +40,65 @@ $(function() {
         'text-decoration': 'none',
         'border-left': 'none',
         'padding': '0 0'
+      });
+    }
+
+    // 見出し2(h2)
+    var h2 = window.sessionStorage.getItem(['h2']);
+    switch (h2){
+      case 'blue':
+        h2DefaultCss();
+        $('.output-markdown h2').css({'color':'#2185d0'});
+        break;
+      case 'underline':
+        h2DefaultCss();
+        $('.output-markdown h2').css({'text-decoration': 'underline'});
+        break;
+      case 'leftLine':
+        h2DefaultCss();
+        $('.output-markdown h2').css({'border-left': 'solid 5px #00b5ad',
+                                      'padding': '0.25em 0.5em'});
+        break;
+      default:
+        h2DefaultCss();
+        break;
+    }
+    function h2DefaultCss() {
+      $('.output-markdown h2').css({
+        'color': '#000',
+        'text-decoration': 'none',
+        'border-left': 'none',
+        'padding': '0 0'
+      });
+    }
+
+    // コード
+    var code = window.sessionStorage.getItem(['code']);
+    switch (code){
+      case 'red':
+        codeDefaultCss();
+        $('.output-markdown pre, .output-markdown p code').css({'background-color': '#FFFAFA',
+                                                                'color': '#db2828'});
+        break;
+      case 'green':
+        codeDefaultCss();
+        $('.output-markdown pre, .output-markdown p code').css({'background-color': '#F5FFFA',
+                                                                'color': '#21ba45'});
+        break;
+      case 'blue':
+        codeDefaultCss();
+        $('.output-markdown pre, .output-markdown p code').css({'background-color': '#F8F8FF',
+                                                                'color': '#2185d0'});
+        break;
+      default:
+        codeDefaultCss();
+        break;
+    }
+    function codeDefaultCss() {
+      $('.output-markdown pre, .output-markdown p code').css({
+        'background-color': '#f7f7f7',
+        'color': '#000',
+        'padding': '9.5px'
       });
     }
   }
