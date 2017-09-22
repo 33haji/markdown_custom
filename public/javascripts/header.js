f7c82e40ebf739c8a4c4a5d8577329b7c62f9853
33haji <one_for_all_0828@yahoo.co.jp>
2017-09-22 15:20:53 +0900

    Markdownのstyleをcssから読み込む形式に修正

--- public/javascripts/header.js
+++ public/javascripts/header.js
@@ -30,81 +30,81 @@ $(function(){
   // Markdownのcssの変更(実際の変更処理はindex.js)
   initializeMarkdown();
   // 見出し1(h1)
-  $(".h1-default").on('click', function(){
+  $(".menu-h1-default").on('click', function(){
     editMarkdownCss("h1", "default");
   });
-  $(".h1-red").on('click', function(){
+  $(".menu-h1-red").on('click', function(){
     editMarkdownCss("h1", "red");
   });
-  $(".h1-underline").on('click', function(){
+  $(".menu-h1-underline").on('click', function(){
     editMarkdownCss("h1", "underline");
   });
-  $(".h1-leftLine").on('click', function(){
+  $(".menu-h1-leftLine").on('click', function(){
     editMarkdownCss("h1", "leftLine");
   });
   // 見出し2(h2)
-  $(".h2-default").on('click', function(){
+  $(".menu-h2-default").on('click', function(){
     editMarkdownCss("h2", "default");
   });
-  $(".h2-blue").on('click', function(){
+  $(".menu-h2-blue").on('click', function(){
     editMarkdownCss("h2", "blue");
   });
-  $(".h2-underline").on('click', function(){
+  $(".menu-h2-underline").on('click', function(){
     editMarkdownCss("h2", "underline");
   });
-  $(".h2-leftLine").on('click', function(){
+  $(".menu-h2-leftLine").on('click', function(){
     editMarkdownCss("h2", "leftLine");
   });
   // コード
-  $(".code-default").on('click', function(){
+  $(".menu-code-default").on('click', function(){
     editMarkdownCss("code", "default");
   });
-  $(".code-red").on('click', function(){
+  $(".menu-code-red").on('click', function(){
     editMarkdownCss("code", "red");
   });
-  $(".code-green").on('click', function(){
+  $(".menu-code-green").on('click', function(){
     editMarkdownCss("code", "green");
   });
-  $(".code-blue").on('click', function(){
+  $(".menu-code-blue").on('click', function(){
     editMarkdownCss("code", "blue");
   });
   // 強調
-  $(".strong-default").on('click', function(){
+  $(".menu-strong-default").on('click', function(){
     editMarkdownCss("strong", "default");
   });
-  $(".strong-underline").on('click', function(){
+  $(".menu-strong-underline").on('click', function(){
     editMarkdownCss("strong", "underline");
   });
-  $(".strong-red").on('click', function(){
+  $(".menu-strong-red").on('click', function(){
     editMarkdownCss("strong", "red");
   });
-  $(".strong-background").on('click', function(){
+  $(".menu-strong-background").on('click', function(){
     editMarkdownCss("strong", "background");
   });
   // 初期化
   function initializeMarkdown(){
     // 見出し(h1)
     window.sessionStorage.setItem(['h1'],['default']);
-    $(".h1-default").addClass('active');
+    $(".menu-h1-default").addClass('active');
     // 見出し2(h2)
     window.sessionStorage.setItem(['h2'],['default']);
-    $(".h2-default").addClass('active');
+    $(".menu-h2-default").addClass('active');
     // コード
     window.sessionStorage.setItem(['code'],['default']);
-    $(".code-default").addClass('active');
+    $(".menu-code-default").addClass('active');
     // 強調
     window.sessionStorage.setItem(['strong'],['default']);
-    $(".strong-default").addClass('active');
+    $(".menu-strong-default").addClass('active');
   }
   // オプションの変更処理
   function editMarkdownCss(tag, element){
     window.sessionStorage.setItem([tag],[element]);
-    addActive(tag, "."+ tag +"-"+ element);
+    addActive(tag, ".menu-"+ tag +"-"+ element);
     refreshMarkdown();
   }
   // メニューにavtiveクラスを付与
   function addActive(tag, selector){
-    $("[class*='"+ tag +"-']").removeClass('active');
+    $("[class*='menu-"+ tag +"-']").removeClass('active');
     $(selector).addClass('active');
   }
   // メニューをクリック直後にstyleを反映させるための処理
--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -9,124 +9,40 @@ app.controller('markdownAreaCtrl', function($scope) {
 /* Markdownのstyleを変換 */
 $(function() {
   $('#output-markdown-contents').on('DOMSubtreeModified propertychange', function() {
-    addCustomCss();
+    addCustomMarkdownClass();
   });
 
-  // 各要素ごとにstyleを適用(どのstyleかという情報はsessionから取得)
-  function addCustomCss() {
+  /**
+   * 各要素ごとにclssを追加(どのclassかという情報はsessionから取得)
+   */
+  function addCustomMarkdownClass() {
     // 見出し1(h1)
     var h1 = window.sessionStorage.getItem(['h1']);
-    switch (h1){
-      case 'red':
-        h1DefaultCss();
-        $('.output-markdown h1').css({'color':'#db2828'});
-        break;
-      case 'underline':
-        h1DefaultCss();
-        $('.output-markdown h1').css({'text-decoration': 'underline'});
-        break;
-      case 'leftLine':
-        h1DefaultCss();
-        $('.output-markdown h1').css({'border-left': 'solid 5px #f2711c',
-                                      'padding': '0.25em 0.5em'});
-        break;
-      default:
-        h1DefaultCss();
-        break;
-    }
-    function h1DefaultCss() {
-      $('.output-markdown h1').css({
-        'color': '#000',
-        'text-decoration': 'none',
-        'border-left': 'none',
-        'padding': '0 0'
-      });
-    }
+    addAndRemoveClass('h1', h1);
 
     // 見出し2(h2)
     var h2 = window.sessionStorage.getItem(['h2']);
-    switch (h2){
-      case 'blue':
-        h2DefaultCss();
-        $('.output-markdown h2').css({'color':'#2185d0'});
-        break;
-      case 'underline':
-        h2DefaultCss();
-        $('.output-markdown h2').css({'text-decoration': 'underline'});
-        break;
-      case 'leftLine':
-        h2DefaultCss();
-        $('.output-markdown h2').css({'border-left': 'solid 5px #00b5ad',
-                                      'padding': '0.25em 0.5em'});
-        break;
-      default:
-        h2DefaultCss();
-        break;
-    }
-    function h2DefaultCss() {
-      $('.output-markdown h2').css({
-        'color': '#000',
-        'text-decoration': 'none',
-        'border-left': 'none',
-        'padding': '0 0'
-      });
-    }
+    addAndRemoveClass('h2', h2);
 
     // コード
     var code = window.sessionStorage.getItem(['code']);
-    switch (code){
-      case 'red':
-        codeDefaultCss();
-        $('.output-markdown pre, .output-markdown p code').css({'background-color': '#FFFAFA',
-                                                                'color': '#db2828'});
-        break;
-      case 'green':
-        codeDefaultCss();
-        $('.output-markdown pre, .output-markdown p code').css({'background-color': '#F5FFFA',
-                                                                'color': '#21ba45'});
-        break;
-      case 'blue':
-        codeDefaultCss();
-        $('.output-markdown pre, .output-markdown p code').css({'background-color': '#F8F8FF',
-                                                                'color': '#2185d0'});
-        break;
-      default:
-        codeDefaultCss();
-        break;
-    }
-    function codeDefaultCss() {
-      $('.output-markdown pre, .output-markdown p code').css({
-        'background-color': '#f7f7f7',
-        'color': '#000',
-        'padding': '9.5px'
-      });
-    }
+    addAndRemoveClass('code', code);
 
     // 強調
     var strong = window.sessionStorage.getItem(['strong']);
-    switch (strong){
-      case 'underline':
-        strongDefaultCss();
-        $('.output-markdown strong').css({'border-bottom': '2px solid #ff3333'});
-        break;
-      case 'red':
-        strongDefaultCss();
-        $('.output-markdown strong').css({'color': '#db2828'});
-        break;
-      case 'background':
-        strongDefaultCss();
-        $('.output-markdown strong').css({'background-color': '#ffff66'});
-        break;
-      default:
-        strongDefaultCss();
-        break;
-    }
-    function strongDefaultCss() {
-      $('.output-markdown strong').css({
-        'border-style': 'none',
-        'color': '#000',
-        'background-color': '#FFF'
-      });
-    }
+    addAndRemoveClass('strong', strong);
+  }
+
+  /**
+   * 既存のclassを削除し、新たなmarkdown用のクラスを付与する
+   * @param {string} tag          タグ(例：h1)
+   * @param {string} classElement 要素名(例：default)
+   */
+  function addAndRemoveClass(tag, classElement) {
+    $('.output-markdown ' + tag).removeClass(function(index, className) {
+      reg = new RegExp('\\b'+ tag +'-\\S+', 'g');
+      return (className.match(reg) || []).join(' ');
+    });
+    $('.output-markdown ' + tag).addClass(tag + '-' + classElement);
   }
 });
